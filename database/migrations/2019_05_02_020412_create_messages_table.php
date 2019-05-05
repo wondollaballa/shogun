4113c0c0a67d3f1c53adc991c41f3132e564ca65
Wondo Choung <onedough83@gmail.com>
2019-05-05 01:11:05 -0500

    fix length of message

--- database/migrations/2019_05_02_020412_create_messages_table.php
+++ database/migrations/2019_05_02_020412_create_messages_table.php
@@ -17,7 +17,7 @@ class CreateMessagesTable extends Migration
             $table->increments('id');
             $table->string('name');
             $table->string('email');
-            $table->string('message');
+            $table->text('message');
             $table->dateTime('viewed')->nullable();
             $table->tinyInteger('status')->default(1);
             $table->softDeletes();
