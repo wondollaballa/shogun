d2f07df1d78a68a270a4f4b5fc3836fb7eb13a13
Wondo Choung <onedough83@gmail.com>
2019-06-08 19:14:20 -0500

    fest

--- database/seeds/HomeContentsTableSeeder.php
+++ database/seeds/HomeContentsTableSeeder.php
@@ -16,40 +16,40 @@ class HomeContentsTableSeeder extends Seeder
             'hasHtml' => false,
             'name' => 'header',
             'image_src' => '../images/hibachi-2.jpg',
-            'image_rotate'=>null,
+            'image_rotate'=> null,
             'html_content' => null,
             'created_at' => Carbon::now(),
             'updated_at' => Carbon::now()
         ]);
-        DB::table('home_contents')->insert([
-            'hasImage' => true,
-            'hasHtml' => true,
-            'name' => 'aboutus',
-            'image_src' => '../images/hibachi-6.jpg',
-            'image_rotate' => null,
-            'html_content' => null,
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now()
-        ]);
-        DB::table('home_contents')->insert([
-            'hasImage' => true,
-            'hasHtml' => false,
-            'name' => 'theexperience',
-            'image_src' => '../images/hibachi-7.jpg',
-            'image_rotate' => null,
-            'html_content' => null,
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now()
-        ]);
-        DB::table('home_contents')->insert([
-            'hasImage' => false,
-            'hasHtml' => true,
-            'name' => 'footer',
-            'image_src' => null,
-            'image_rotate' => null,
-            'html_content' => null,
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now()
-        ]);
+        // DB::table('home_contents')->insert([
+        //     'hasImage' => true,
+        //     'hasHtml' => true,
+        //     'name' => 'aboutus',
+        //     'image_src' => '../images/hibachi-6.jpg',
+        //     'image_rotate' => null,
+        //     'html_content' => null,
+        //     'created_at' => Carbon::now(),
+        //     'updated_at' => Carbon::now()
+        // ]);
+        // DB::table('home_contents')->insert([
+        //     'hasImage' => true,
+        //     'hasHtml' => false,
+        //     'name' => 'theexperience',
+        //     'image_src' => '../images/hibachi-7.jpg',
+        //     'image_rotate' => null,
+        //     'html_content' => null,
+        //     'created_at' => Carbon::now(),
+        //     'updated_at' => Carbon::now()
+        // ]);
+        // DB::table('home_contents')->insert([
+        //     'hasImage' => false,
+        //     'hasHtml' => true,
+        //     'name' => 'footer',
+        //     'image_src' => null,
+        //     'image_rotate' => null,
+        //     'html_content' => null,
+        //     'created_at' => Carbon::now(),
+        //     'updated_at' => Carbon::now()
+        // ]);
     }
 }
